4f305a7db79ee3af9db0239674a67f47c3074cc5
Robertleoj <robert.leo.jonsson@gmail.com>
2022-06-12 02:44:33 +0000

    delete unused stuff

--- build_run_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker rm -f report-server
-docker build -t report-server .
-docker run -d --name report-server -p 4000:80 --mount source=report_data,destination=/code/report_data report-server
